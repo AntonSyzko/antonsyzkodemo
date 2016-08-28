1a72f69fe5478a72f3d9a4db8b16b9b2d96e6a38
AntonSyzko <antonio.shizko@gmail.com>
2016-08-28 19:36:13 +0100

    q

--- src/main/resources/DBBackup.sql
+++ src/main/resources/DBBackup.sql
@@ -46,13 +46,13 @@ VALUES ('DBA');
 
 /* Populate one Admin User which will further create other users for the application using GUI */
 INSERT INTO APP_USER(sso_id, password, first_name, last_name, email, phone,joining_date,description)
-VALUES ('testAdmin','$2a$10$5Hw52OpxBwVieaAM3WSU0umdpphZNe2osZfap1im3fXPIkjYl0SE2', 'testAdmin','testAdmin','testAdmin@gmail.com','442071234337','2015-12-12','test admin message');
-
+VALUES ('test','$2a$10$mCblM7QrmnRn4nHQLo79o.062pzTIDYkHIRJ1VhIGFWhGGc3R4CYS
+', 'test','test','test@gmail.com','442071234337','2015-12-12','test admin message');
 
 /* Populate JOIN Table */
 INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
-  SELECT user.id, profile.id FROM app_user user, user_profile profile
-  where user.sso_id='testAdmin' and profile.type='ADMIN';
+  SELECT "user".id, profile.id FROM app_user "user", user_profile profile
+  where "user".sso_id='test' and profile.type='ADMIN';
 
 /* Create persistent_logins Table used to store rememberme related stuff*/
 CREATE TABLE persistent_logins (
